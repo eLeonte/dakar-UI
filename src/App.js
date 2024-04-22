f3f9963c4737a3cd3b973a91bce1383e1d0474aa
LeonteAndrei <andrei.leonte@supplypoint.com>
2024-04-22 23:31:20 +0300

    Dakar-1: Initial development to render what the API's returns

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./Home";
+import { Project } from "./Project";
+import { TestCase } from "./TestCase";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">
+          {" "}
+          React JS Frontend
+        </h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item- m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/project"
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item- m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/testcase"
+              >
+                Test Cases
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/testcase" element={<TestCase />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
