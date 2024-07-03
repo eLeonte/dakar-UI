f8bda21f3deda1701b843fec10b1b7ad0baa1dc4
LeonteAndrei <andrei.leonte@supplypoint.com>
2024-07-04 00:27:10 +0300

    Dakar-3: Pushed latest changes

--- src/Home.js
+++ src/Home.js
@@ -7,62 +7,40 @@ export class Home extends Component {
     return (
       <div className="d-flex align-items-center min-vh-1000">
         <div className="container">
+          <div className="row mt-5">
+            <div className="col-12">
+              <h5 className="section-title">
+                Get started by pressing the "Project" button
+              </h5>
+            </div>
+            <div className="col-12 mb-4 d-flex">
+              {/* <img
+                src="/Images/ProjectButton1.png"
+                className="img-fluid me-3"
+                alt="Screenshot 1"
+              /> */}
+              <p className="align-self-center"></p>
+            </div>
+          </div>
           <div className="row justify-content-center">
             <div className="col-md-8">
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">Welcome to the Home Page</h5>
+                  <h5 className="card-title">About the app</h5>
                   <p className="card-text">
-                    This web application is meant for organizing the Testing
-                    Teams within the Project and for each Tester to be able to
-                    generate a test plan in which he can have different test
-                    cases with well-defined steps to follow.
+                    Welcome to our project management platform designed
+                    specifically for testing teams. <br /> This application
+                    enables each tester to effortlessly create and organize test
+                    plans.
+                    <br />
+                    You can generate comprehensive test cases with clear,
+                    step-by-step instructions, ensuring thorough testing and
+                    efficient workflow management. Start exploring and
+                    streamline your testing process today!
                   </p>
                 </div>
               </div>
             </div>
-          </div>
-
-          {/* Getting Started Section */}
-          <div className="row mt-5">
-            <div className="col-12">
-              <h5 className="section-title">
-                Get Started by pressing the "Project" button
-              </h5>
-            </div>
-            {/* <div className="col-12 mb-4 d-flex">
-              <img
-                src="/Images/ProjectPage.png"
-                className="img-fluid me-3"
-                alt="Screenshot 1"
-              />
-              <p className="align-self-center">Projects</p>
-            </div> */}
-            {/* <div className="col-md-6 mb-4 d-flex">
-              <img
-                src="/Images/TestCasePage.png"
-                className="img-fluid me-3"
-                alt="Screenshot 2"
-              />
-              <p className="align-self-center">Test Cases</p>
-            </div> */}
-            {/* <div className="col-md-6 mb-4 d-flex">
-              <img
-                src="/Images/TestSteps.png"
-                className="img-fluid me-3"
-                alt="Screenshot 3"
-              />
-              <p className="align-self-center">Test Steps</p>
-            </div> */}
-
-            {/* <div className="col-md-6 mb-4 d-flex">
-              <img
-                src="/Images/TestRunPage.png"
-                className="img-fluid me-3"
-                alt="Screenshot 4"
-              />
-              <p className="align-self-center">Test Run</p>
-            </div> */}
           </div>
         </div>
       </div>
--- src/Project.js
+++ src/Project.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { variables } from "./Variables.js";
-// import { Link, Route, Routes } from "react-router-dom";
-// import CreateTestRun from "./CreateTestRun.js";
 import "./App.css";
 
 export class Project extends Component {
@@ -735,7 +733,7 @@ export class Project extends Component {
       return <div className="alert alert-danger">Error: {error.message}</div>;
     }
 
-    // Conditional rendering based on isTestRunMode
+    // Daca proiectul este selectat si Test Run este activ
     if (isTestRunMode && selectedProject) {
       if (selectedTestCase) {
         return (
@@ -753,7 +751,6 @@ export class Project extends Component {
                   <th>StepId</th>
                   <th>Test Step</th>
                   <th>Expected Result</th>
-                  {/* <th>Options</th> */}
                 </tr>
               </thead>
               <tbody>
@@ -762,68 +759,12 @@ export class Project extends Component {
                     <td>{step.StepID}</td>
                     <td>{step.StepDescription}</td>
                     <td>{step.ExpectedResult}</td>
-                    {/* <td>
-                      {" "}
-                      <button
-                        type="button"
-                        className="btn btn-light mr-1"
-                        data-bs-toggle="modal"
-                        data-bs-target="#testCaseStepsModal"
-                        onClick={() => this.editStepClick(step)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-pencil-square"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                          <path
-                            fillRule="evenodd"
-                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                          />
-                        </svg>
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-light mr-1"
-                        onClick={() =>
-                          this.deleteStepClick(step.TestCaseId, step.StepID)
-                        }
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-trash-fill"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                        </svg>
-                      </button>
-                    </td> */}
                   </tr>
                 ))}
-                {/* <tr>
-                  <td colSpan="4" className="text-center">
-                    <button
-                      type="button"
-                      className="btn btn-primary wide-button new-btn"
-                      data-bs-toggle="modal"
-                      data-bs-target="#testCaseStepsModal"
-                      onClick={this.addStepClick}
-                    >
-                      + New
-                    </button>
-                  </td>
-                </tr> */}
               </tbody>
             </table>
 
-            {/* Test Case Steps Modal */}
+            {/* Modal Test Case Steps */}
             <div
               className="modal fade"
               id="testCaseStepsModal"
@@ -1041,7 +982,6 @@ export class Project extends Component {
               </tbody>
             </table>
 
-            {/* Test Case Steps Modal */}
             <div
               className="modal fade"
               id="testCaseStepsModal"
